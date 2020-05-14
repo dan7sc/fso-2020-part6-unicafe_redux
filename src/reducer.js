49f252b1455167aa76ad4284f30e4c03012045fd
dan7sc <su7cheng@gmail.com>
2020-05-13 22:53:41 -0300

    Add solution to exercise 6.1: unicafe revisited, step1

--- src/reducer.js
+++ src/reducer.js
@@ -7,17 +7,16 @@ const initialState = {
 const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
-    case 'GOOD':
-      return state
-    case 'OK':
-      return state
-    case 'BAD':
-      return state
-    case 'ZERO':
-      return state
-    default: return state
+  case 'GOOD':
+    return { ...state, good: state.good + 1 }
+  case 'OK':
+    return { ...state, ok: state.ok + 1 }
+  case 'BAD':
+    return { ...state, bad: state.bad + 1 }
+  case 'ZERO':
+    return initialState
+  default: return initialState
   }
-  
 }
 
 export default counterReducer
